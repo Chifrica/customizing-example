00ca7fa7ede2a62e4c269ac4c8ef1bf80eb5fee5
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-06-30 15:33:15 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
+import Intro1 from './components/Intro1';
 
 function App() {
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+
+       </div>
+       /*{ <header className="App-header">{ }
+   //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> }*/
+    
   );
 }
 
