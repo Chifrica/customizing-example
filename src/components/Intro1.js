f28fafaa8568a92efc5f3512c031cba933cac943
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-07-04 08:34:18 +0100

    My second commit

--- src/components/Intro1.js
+++ src/components/Intro1.js
@@ -1,7 +1,7 @@
-function Intro1(){
+function Intro1(props){
     return (
         <div className="blog-post-intro">
-            <h2>I've become a React developer!</h2>
+            <h2>I've become a {props.react}</h2>
             <div>
                 <p>I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
                 <p className="link">Read more...</p>
